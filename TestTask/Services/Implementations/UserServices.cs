e463de2ddaf0778064beee8e14f3ff8d14dee9fa
Admin <Admin@DESKTOP-PEHN76O>
2024-05-29 00:25:37 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/UserServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        public UserService(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+        private ApplicationDbContext Context { get; }
+        public Task<User> GetUser()
+        {
+            return Context.Users
+                 .OrderByDescending(user => user.Orders
+                                                .Where(order => order.CreatedAt.Year == 2003 && order.Status == OrderStatus.Delivered)
+                                                .Select(order => order.Quantity * order.Price))
+                 .FirstOrDefaultAsync();
+                                        
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return Context.Users
+                .Where(user => user.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+        }
+
+
+    }
+}
